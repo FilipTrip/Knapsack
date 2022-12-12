63fd360606b9bb6d8a3c372a1f3b7b3eed0fa38a
FilipTrip <81421618+FilipTrip@users.noreply.github.com>
2022-12-12 14:15:50 +0100

    Relative value

--- Knapsack/Assets/Scripts/Item.cs
+++ Knapsack/Assets/Scripts/Item.cs
@@ -7,15 +7,17 @@ public struct Item
 {
     public float weight;
     public float value;
+    public float relativeValue;
 
     public Item(float weight, float value)
     {
         this.weight = weight;
         this.value = value;
+        relativeValue = value / weight;
     }
 
     public override string ToString()
     {
-        return "w: " + weight.ToString("0.00") + " v: " + value.ToString("0.00");
+        return "w: " + weight.ToString("0.00") + " v: " + value.ToString("0.00") + " v/w: " + relativeValue.ToString("0.00");
     }
 }
--- Knapsack/Assets/Scripts/ItemGenerator.cs
+++ Knapsack/Assets/Scripts/ItemGenerator.cs
@@ -8,6 +8,8 @@ public class ItemGenerator : MonoBehaviour
     [Header("Values")]
 
     [SerializeField] private int itemCount;
+    [SerializeField] private float minWeight;
+    [SerializeField] private float minValue;
     [SerializeField] private float maxWeight;
     [SerializeField] private float maxValue;
     [SerializeField] private AnimationCurve weightDistribution;
@@ -24,10 +26,13 @@ public class ItemGenerator : MonoBehaviour
     {
         items = new Item[itemCount];
 
+        float value, weight;
+
         for (int i = 0; i < itemCount; ++i)
         {
-            items[i].weight = weightDistribution.Evaluate(Random.Range(0f, 1f));
-            items[i].value  = valueDistribution .Evaluate(Random.Range(0f, 1f));
+            weight = minWeight + (maxWeight - minWeight) * weightDistribution.Evaluate(Random.Range(0f, 1f));
+            value  = minValue  + (maxValue  - minValue)  * valueDistribution .Evaluate(Random.Range(0f, 1f));
+            items[i] = new Item(weight, value);
         }
 
         Finished.Invoke();
